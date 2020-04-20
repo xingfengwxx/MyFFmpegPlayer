acaacab49c28970e00653571550c08be2be8d1cf
xingfengwxx <1099420259@qq.com>
2020-04-20 15:23:17 +0800

    更换icon，显示当前播放状态

--- app/src/main/java/com/wangxingxing/myffmpegplayer/MainActivity.java
+++ app/src/main/java/com/wangxingxing/myffmpegplayer/MainActivity.java
@@ -1,10 +1,15 @@
 package com.wangxingxing.myffmpegplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
@@ -16,10 +21,17 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SurfaceView surfaceView;
-//    public static final String PATH = "rtmp://58.200.131.2:1935/livetv/hunantv";
+    //    public static final String PATH = "rtmp://58.200.131.2:1935/livetv/hunantv";
     private final static String PATH = Environment.getExternalStorageDirectory() + File.separator + "demo.mp4";
-    MyPlayer myPlayer;
+
+    public static final int MSG_UPDATE_TEXT = 1;
+
+    private SurfaceView surfaceView;
+    private TextView tvInfo;
+
+    private MyPlayer myPlayer;
+
+    private long mFirstPressTime = 0;
 
     private String[] permissions = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -34,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         checkPermission();
 
         surfaceView = findViewById(R.id.surface_view);
+        tvInfo = findViewById(R.id.tv_info);
 
         initPlayer();
     }
@@ -43,16 +56,13 @@ public class MainActivity extends AppCompatActivity {
         myPlayer.setSurfaceView(surfaceView);
         myPlayer.setDataSource(PATH);
 
+        sendMsg("FFmpeg version:" + myPlayer.getFFmpegVersion());
+
         myPlayer.setOnPreparedListener(new MyPlayer.OnPreparedListener() {
             @Override
             public void onPrepared() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ToastUtils.showShort("准备好了，开始播放...");
-                    }
-                });
                 LogUtils.i("准备好了，开始播放...");
+                sendMsg("准备好了，开始播放...");
 
                 // 准备成功之后，开始播放 视频 音频
                 myPlayer.start();
@@ -60,12 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(final String errorText) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ToastUtils.showShort("发生错误，请查阅：" + errorText);
-                    }
-                });
+                sendMsg("发生错误，请查阅：" + errorText);
             }
         });
     }
@@ -88,9 +93,46 @@ public class MainActivity extends AppCompatActivity {
         myPlayer.release();
     }
 
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - mFirstPressTime < 2000) {
+            super.onBackPressed();
+        } else {
+            mFirstPressTime = System.currentTimeMillis();
+            ToastUtils.showShort("再按一次返回键退出");
+        }
+    }
+
     private void checkPermission() {
         if (!PermissionUtils.isGranted(permissions)) {
             PermissionUtils.permission(permissions).request();
         }
     }
+
+    private void sendMsg(String str) {
+        Message msg = new Message();
+        msg.what = MSG_UPDATE_TEXT;
+        msg.obj = str;
+        mHandler.sendMessage(msg);
+    }
+
+    private void updateInfo(String str) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(tvInfo.getText());
+        sb.append("\n");
+        sb.append(str);
+        tvInfo.setText(sb.toString());
+    }
+
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case MSG_UPDATE_TEXT:
+                    updateInfo(msg.obj.toString());
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 }
