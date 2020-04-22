d89a15feacfc0ec4417edbafb38c9772c8c49920
xingfengwxx <1099420259@qq.com>
2020-04-22 17:06:42 +0800

    添加MP4和RTMP切换功能

--- app/src/main/java/com/wangxingxing/myffmpegplayer/MainActivity.java
+++ app/src/main/java/com/wangxingxing/myffmpegplayer/MainActivity.java
@@ -15,8 +15,10 @@ import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.PermissionUtils;
+import com.blankj.utilcode.util.SPUtils;
 import com.blankj.utilcode.util.ToastUtils;
 
 import java.io.File;
@@ -25,12 +27,15 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     private static final String TAG = "MainActivity";
 
-    //    public static final String PATH = "rtmp://58.200.131.2:1935/livetv/hunantv";
-    private final static String PATH = Environment.getExternalStorageDirectory() + File.separator + "demo.mp4";
+    public static final String PATH_HUNANTV_RTMP = "rtmp://58.200.131.2:1935/livetv/hunantv";
+    private final static String PATH_LOCAL = Environment.getExternalStorageDirectory() + File.separator + "demo.mp4";
 
     public static final int MSG_UPDATE_TEXT = 1;
     public static final int MSG_UPDATE_PROGRESS = 2;
     public static final int MSG_IS_SHOW_SEEKBAR = 3;
+
+    // 0:MP4; 1:RTMP
+    public static final String SP_KEY_TYPE = "type";
 
     private SurfaceView surfaceView;
     private TextView tvInfo;
@@ -43,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private boolean isSeek;
 
     private int duration = 0;
+    private String videoPath = "";
 
     private String[] permissions = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -61,15 +67,23 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         seekBar = findViewById(R.id.seek_bar);
         seekBar.setOnSeekBarChangeListener(this);
 
+
         initPlayer();
     }
 
     private void initPlayer() {
         myPlayer = new MyPlayer();
         myPlayer.setSurfaceView(surfaceView);
-        myPlayer.setDataSource(PATH);
+        int playType = SPUtils.getInstance().getInt(SP_KEY_TYPE, 0);
+        if (playType == 0) {
+            videoPath = PATH_LOCAL;
+        } else {
+            videoPath = PATH_HUNANTV_RTMP;
+        }
+        myPlayer.setDataSource(videoPath);
 
-        sendInfoMsg("FFmpeg version:" + myPlayer.getFFmpegVersion());
+        sendInfoMsg("ffmpeg version:" + myPlayer.getFFmpegVersion());
+        sendInfoMsg("ffmpeg player init, play url: " + videoPath);
 
         myPlayer.setOnPreparedListener(new MyPlayer.OnPreparedListener() {
             @Override
@@ -112,19 +126,25 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     protected void onResume() {
         super.onResume();
-        myPlayer.prepare();
+        if (myPlayer != null) {
+            myPlayer.prepare();
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        myPlayer.stop();
+        if (myPlayer != null) {
+            myPlayer.stop();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        myPlayer.release();
+        if (myPlayer != null) {
+            myPlayer.release();
+        }
     }
 
     @Override
@@ -181,6 +201,16 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         seekBar.setProgress(progress);
     }
 
+    public void playMP4(View view) {
+        SPUtils.getInstance().put(SP_KEY_TYPE, 0);
+        AppUtils.relaunchApp(true);
+    }
+
+    public void playRTMP(View view) {
+        SPUtils.getInstance().put(SP_KEY_TYPE, 1);
+        AppUtils.relaunchApp(true);
+    }
+
     private Handler mHandler = new Handler(Looper.getMainLooper()) {
         @Override
         public void handleMessage(@NonNull Message msg) {
@@ -203,12 +233,14 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         }
     };
 
-    /** SeekBar callback start
+    /**
+     * SeekBar callback start
      * seek 的核心思路1
      * 跟随播放进度自动刷新进度：拿到每个时间点相对总播放时长的百分比进度 progress
      * 1，总时间 getDurationNative
      * 2，当前播放时间: 随播放进度动态变化的
-     * ===================================================================================*/
+     * ===================================================================================
+     */
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
@@ -221,6 +253,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     /**
      * 当用户手 离开拖动条的时候，就在这一刻：把拖动条的值 告诉给底层
+     *
      * @param seekBar
      */
     @Override
@@ -234,14 +267,15 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         int playProgress = seekBarProgress * duration / 100; // 把拖动条的值，变成播放的进度时长
 
         /*
-        * 将播放进度传给底层 ffmpeg
-        * playProgress == 时长 native只认识时长，不认识拖动值
-        *
-        * seek 的核心思路2
-        * 手动拖动进度条，要能跳到指定的播放进度  av_seek_frame
-        * */
+         * 将播放进度传给底层 ffmpeg
+         * playProgress == 时长 native只认识时长，不认识拖动值
+         *
+         * seek 的核心思路2
+         * 手动拖动进度条，要能跳到指定的播放进度  av_seek_frame
+         * */
         LogUtils.i("playProgress: " + playProgress);
         myPlayer.seekTo(playProgress);
     }
+
     /** SeekBar callback end =============================================================*/
 }
